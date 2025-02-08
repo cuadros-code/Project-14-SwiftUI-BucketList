9d5658931a67db4bf0110c06dc72897f979b2d17
kevin cuadros <kevindavidcuadros@gmail.com>
2025-02-08 14:15:40 -0500

    Initial Commit

---
+++ Project-14-SwiftUI-BucketList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-14-SwiftUI-BucketList
+//
+//  Created by Kevin Cuadros on 8/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-14-SwiftUI-BucketList/Project_14_SwiftUI_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_14_SwiftUI_BucketListApp.swift
+//  Project-14-SwiftUI-BucketList
+//
+//  Created by Kevin Cuadros on 8/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_14_SwiftUI_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
