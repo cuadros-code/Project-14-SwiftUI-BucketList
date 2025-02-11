f42a9cb7b5c84e10e65b5f57fe90f3b3423a6051
kevin cuadros <kevindavidcuadros@gmail.com>
2025-02-10 20:12:12 -0500

    create map

--- Project-14-SwiftUI-BucketList/ContentView.swift
+++ Project-14-SwiftUI-BucketList/ContentView.swift
@@ -6,11 +6,51 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
     var body: some View {
-        Text("")
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid(elevation: .realistic))
+                .onMapCameraChange { context in
+                    print(context.region)
+                }
+            
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(
+                                latitude: 48.8566,
+                                longitude: 2.3522
+                            ),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+                
+                Button("Tokio") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(
+                                latitude: 35.6897,
+                                longitude: 139.6922
+                            ),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+            }
+        }
     }
 }
 
