d1784f2ee02e1676f553231068197eb8cc0a46af
kevin cuadros <kevindavidcuadros@gmail.com>
2025-02-24 10:34:53 -0500

    face id

---
+++ FaceIDView.swift
@@ -0,0 +1,18 @@
+//
+//  FaceIDView.swift
+//  Project-14-SwiftUI-BucketList
+//
+//  Created by Kevin Cuadros on 24/02/25.
+//
+
+import SwiftUI
+
+struct FaceIDView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaceIDView()
+}
