6c488f2575ee206ee1fed5e2a714527ca925ace9
kevin cuadros <kevindavidcuadros@gmail.com>
2025-03-05 21:38:07 -0500

    add marker into the map

--- Project-14-SwiftUI-BucketList/ContentView.swift
+++ Project-14-SwiftUI-BucketList/ContentView.swift
@@ -20,14 +20,33 @@ struct ContentView: View {
         )
     )
     
+    @State private var locations = [Location]()
+    
     
     var body: some View {
         MapReader { proxy in
-            Map(initialPosition: startPosition)
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Marker(
+                        location.name,
+                        coordinate: CLLocationCoordinate2D(
+                            latitude: location.latitude,
+                            longitude: location.longitude
+                        )
+                    )
+                }
+            }
                 .mapStyle(.hybrid)
                 .onTapGesture { position in
                     if let coordinate = proxy.convert(position, from: .local) {
-                        print("\(coordinate)")
+                        let newLocation = Location(
+                            id: UUID(),
+                            name: "New Location",
+                            description: "",
+                            latitude: coordinate.latitude,
+                            longitude: coordinate.longitude
+                        )
+                        locations.append(newLocation)
                     }
                 }
         }
