1a4c1256e435ef35e71cd5c230fbdd4f07324b67
kevin cuadros <kevindavidcuadros@gmail.com>
2025-03-17 20:15:32 -0500

    Sorting Wikipedia results

--- Project-14-SwiftUI-BucketList/EditLocationView.swift
+++ Project-14-SwiftUI-BucketList/EditLocationView.swift
@@ -41,10 +41,8 @@ struct EditLocationView: View {
                         ForEach(pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
-                            
                             + Text(": ") +
-                            
-                            Text("Page Description here")
+                            Text(page.description)
                                 .italic()
                         }
                     case .failed:
@@ -92,7 +90,7 @@ struct EditLocationView: View {
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
             let items = try JSONDecoder().decode(Result.self, from: data)
-            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            pages = items.query.pages.values.sorted()
             loadingState = .loaded
         } catch {
             loadingState = .failed
